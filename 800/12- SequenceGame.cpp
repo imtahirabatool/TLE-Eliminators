7a2232eb613c49af4586a27e0626584a35fb4a99
imtahirabatool <btahira183@gmail.com>
2025-01-11 16:55:14 +0500

    SequenceGame

---
+++ 800/12- SequenceGame.cpp
@@ -0,0 +1,40 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+
+int main(){
+     int t;
+    cin>>t;
+    while(t--)
+    {
+        int n;
+        cin>>n;
+        int b[n];
+        for(int i=0; i<n; i++)
+        {
+            cin>>b[i];
+        }
+        vector<int> ans;
+        ans.push_back(b[0]);
+        for(int i=1; i<n;i++)
+        {
+            if(b[i]>=b[i-1])
+            {
+                ans.push_back(b[i]);
+            }
+            else
+            {
+                ans.push_back(b[i]);
+                ans.push_back(b[i]);
+            }
+        }
+        int k = ans.size();
+        cout<<k<<endl;
+        for(int i=0; i<k; i++)
+        {
+            cout<<ans[i]<<" ";
+        }
+        cout<<endl;
+    }
+    return 0;
+}
